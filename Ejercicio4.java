bc1e3d4c02fa19b130f3a761900248683ba8e1b8
Erika Mutzan <erikamtzmutzan@gmail.com>
2025-03-04 21:00:33 -0600

    feat: se hicieron las correcciones de sintaxis, se agreron equals, break,comentarios

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -1,40 +1,46 @@
-package com.generation;
+//package com.generation; 
+//se probo en eclipse y el package com.generation; funciona al no estar comentado
+//se probo en visual studio code y al comentar el package permite correrlo en la terminal y tambien funciona
 
-public class Codigo4 {
+import java.util.Scanner;
 
-    Scanner s = new Scanner();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    String j1 = s.nextLine();
-    
-    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-    Scanner s2 = new Scanner();
-    String j2 = s.nextLine();
-    
-    if (j1 == j2)) {
-      System.out.println("Empate");
-    } else {
-      int g = 2;
-      switch(j1) {
-        case "piedra":
-          if (j2 == "tijeras") {
-            g = 1;
-          }
+public class Ejercicio4 {
 
-        case "papel":
-          if (j2 == "piedra") {
-            g = 1;
-          
-        case "tijera":
-          if (j2.equals("papel")) {
-            g = 1;
-          }
-          break;
-        default:
-      }
-      System.out.println("Gana el jugador " + g);
-    }
-  
-  
-}
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+	    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
+	    String j1 = s.nextLine();
+	    
+	    Scanner s2 = new Scanner(System.in);
+	    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): "); // se coloco el 2
+	    String j2 = s2.nextLine(); //se agrego el s2
+	    
+
+	    if (j1.equals (j2))  {  //se borro el parentesis, no se ocupa ==
+	      System.out.println("Empate");
+	    } else {
+	      int g = 2;
+	      switch(j1) {
+	        case "piedra":
+	          if (j2.equals("tijeras")) { // se agrego equals
+	            g = 1;
+	          }
+	          break; // se agrego el brak
+	        case "papel":
+	          if (j2.equals ("piedra")) { //se agrego equals
+	            g = 1;
+	          } // se agrego la llave del bloque del codigo
+	          break; //se agrego el break
+	        case "tijera":
+	          if (j2.equals("papel")) {
+	            g = 1;
+	          }
+	          break;
+	        default:
+	      }
+	      System.out.println("Gana el jugador " + g);
+	    }
+
+	}
+
 }
